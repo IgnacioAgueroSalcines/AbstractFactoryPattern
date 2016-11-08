cfacd4383b5d8205f853b8dd2ff9dbbf682aeb09
Ignacio Agüero Salcines <ias61@alumnos.unican.es>
2016-11-08 11:44:09 +0100

    Se han creado las factorias abstractas de visitantes y estrategias

---
+++ AbstractFactoryPattern/AbstractFactoryPattern/Factory/VisitanteAbstractFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisitorPattern;
+
+namespace AbstractFactoryPattern.Factory
+{
+    public abstract class VisitanteAbstractFactory
+    {
+        public abstract IVisitor compacto();
+        public abstract IVisitor extendido();
+
+    }
+}
---
+++ AbstractFactoryPattern/AbstractFactoryPattern/Factory/VisitanteFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisitorPattern;
+
+namespace AbstractFactoryPattern.Factory
+{
+    public class VisitanteFactory : VisitanteAbstractFactory
+    {
+        public override IVisitor compacto()
+        {
+            IVisitor visitante = new ImprimeArbolCompacto();
+            return visitante;
+        }
+
+        public override IVisitor extendido()
+        {
+            IVisitor visitante = new ImprimeArbolExtendido();
+            return visitante;
+        }
+    }
+}
